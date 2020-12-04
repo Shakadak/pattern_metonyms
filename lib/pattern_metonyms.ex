6c1807438ca170ec39ba7e74b123cb0e882ee68c
nathanael <nathanael.pineau@kbrwadventure.com>
2020-12-04 18:01:34 +0100

    lib/pattern_metonyms.ex : improved doc for `pattern`

--- lib/pattern_metonyms.ex
+++ lib/pattern_metonyms.ex
@@ -1,26 +1,181 @@
 defmodule PatternMetonyms do
   @moduledoc """
-  Documentation for `PatternMetonyms`.
+  Reusing the description from the paper:
+
+  Pattern synonyms allow to abstract over patterns used in pattern matching,
+  notably by allowing to use computation instead of being limited to concrete data.
+
+  Pattern metonyms are an implementations of this, but because of the limitation of the language,
+  it can not be considered the same, so metonyms was chosen as a synonym to synonyms.
+
+  Unlike in Haskell, few metonym definitions can be used with `case/2`,
+  other forms of patterns can only be used in combination with `view/2`.
   """
 
   @doc """
-  implicit bidirectional
-  target : pattern just2(a, b) = just({a, b})
-  currently work as is for that kind of complexity
+  Macro used to define pattern metonyms.
 
-  unidirectional
-  target : pattern head(x) <- [x | _]
-  but doesn't work as is
-  "pattern(head(x) <- [x | _])"
+  There are three types of pattern, listed below with examples:
 
-  explicit bidirectional
-  target : pattern polar(r, a) <- (pointPolar -> {r, a}) when polar(r, a) = polarPoint(r, a)
-  but doesn't work as is
-  "pattern (polar(r, a) <- (pointPolar -> {r, a})) when polar(r, a) = polarPoint(r, a) "
+  1) Implicitly Bidirectional
+  The simplest type of pattern, because of its symmetry requirement, it can only
+  be defined using concrete data (or adapted macro). Therefore no computation is
+  allowed, and they are thus compatible with `case` and function heads.
+  They take the form:
+  ```
+  pattern <name>(<[variables]>) = pattern
+  ```
+  where _pattern_ reuses the _variables_
+
+      ```
+      iex> defmodule DoctestTPX do
+      ...>   import PatternMetonyms
+      ...>
+      ...>   pattern ok(x)    = {:ok, x}
+      ...>   pattern error(x) = {:error, x}
+      ...>
+      ...>   pattern cons(x, xs) = [x | xs]
+      ...>
+      ...>   def foo(x) do
+      ...>     view x do
+      ...>       ok(a)    -> a
+      ...>       error(b) -> b
+      ...>     end
+      ...>   end
+      ...>
+      ...>   def bar(x) do
+      ...>     case x do
+      ...>       ok(a)    -> a
+      ...>       error(b) -> b
+      ...>     end
+      ...>   end
+      ...>
+      ...>   def baz(ok(a)   ), do: a
+      ...>   def baz(error(b)), do: b
+      ...>
+      ...>   def mk_ok(x), do: ok(x)
+      ...>
+      ...>   def blorg(xs) do
+      ...>     view xs do
+      ...>       cons(x, xs) -> cons(x, Enum.map(xs, fn x -> -x end))
+      ...>     end
+      ...>   end
+      ...> end
+      iex> DoctestTPX.foo({:ok, :banana})
+      :banana
+      iex> DoctestTPX.foo({:error, :split})
+      :split
+      iex> DoctestTPX.bar({:ok, :peach})
+      :peach
+      iex> DoctestTPX.baz({:error, :melba})
+      :melba
+      iex> DoctestTPX.mk_ok(:melba)
+      {:ok, :melba}
+      iex> DoctestTPX.blorg([1, 2, 3])
+      [1, -2, -3]
+      ```
+
+  2) Unidirectional
+  This type of pattern is read only, it may be used as abstraction over pattern matching on concrete data type
+  that can not be reused to construct data, or as abstraction over views, as explained in `view/2`.
+  They take the form:
+  ```
+  pattern <name>(<[variables]>) <- pattern
+  pattern <name>(<[variables]>) <- (function -> pattern)
+  ```
+  where _pattern_ reuses the _variables_.
+  `(function -> pattern)` is called a view
+
+      ```
+      iex> defmodule DoctestTPY do
+      ...>   import PatternMetonyms
+      ...>
+      ...>   pattern head(x) <- [x | _]
+      ...>
+      ...>   pattern rev_head(x) <- (reverse -> head(x))
+      ...>
+      ...>   def reverse(xs), do: Enum.reverse(xs)
+      ...>
+      ...>   def foo(xs) do
+      ...>     view xs do
+      ...>       head(x) -> x
+      ...>       []      -> []
+      ...>     end
+      ...>   end
+      ...>
+      ...>   def bar(x) do
+      ...>     case x do
+      ...>       head(a) -> a
+      ...>       []      -> []
+      ...>     end
+      ...>   end
+      ...>
+      ...>   def baz(head(a)), do: a
+      ...>
+      ...>   def blorg(xs) do
+      ...>     view xs do
+      ...>       rev_head(x) -> x
+      ...>     end
+      ...>   end
+      ...> end
+      iex> DoctestTPY.foo([1, 2, 3])
+      1
+      iex> DoctestTPY.bar([1, 2, 3])
+      1
+      iex> DoctestTPY.baz([1, 2, 3])
+      1
+      iex> DoctestTPY.blorg([1, 2, 3])
+      3
+      ```
+
+  3) Explicitly bidirectional
+  This type of pattern allows the same kind of abstraction as unidirectional one, but also permit defining
+  how to construct data from computation (if necessary).
+  The take the form:
+  ```
+  pattern (<name>(<[variables]>) <- (function -> pattern)) when <name>(<[variables]>) = builder
+  ```
+  where _pattern_ and _builder_ reuse the _variables_.
+  `(function -> pattern)` is called a view
+
+      ```
+      iex> defmodule DoctestTPZ do
+      ...>   import PatternMetonyms
+      ...>
+      ...>   pattern (snoc(x, xs) <- (unsnoc -> {x, xs}))
+      ...>     when snoc(x, xs) = Enum.reverse([x | Enum.reverse(xs)])
+      ...>
+      ...>   defp unsnoc([]), do: :error
+      ...>   defp unsnoc(xs) do
+      ...>     [x | rev_tail] = Enum.reverse(xs)
+      ...>     {x, Enum.reverse(rev_tail)}
+      ...>   end
+      ...>
+      ...>   def foo(xs) do
+      ...>     view xs do
+      ...>       snoc(x, _) -> x
+      ...>       []      -> []
+      ...>     end
+      ...>   end
+      ...>
+      ...>   def bar(xs) do
+      ...>     view xs do
+      ...>       snoc(x, xs) -> snoc(-x, xs)
+      ...>       []      -> []
+      ...>     end
+      ...>   end
+      ...> end
+      iex> DoctestTPZ.foo([1, 2, 3])
+      3
+      iex> DoctestTPZ.bar([1, 2, 3])
+      [1, 2, -3]
+      ```
+
+  Patterns using a view can not be used with `case`.
+
+  Currently does not support remote calls.
   """
   # implicit bidirectional
-  # lhs = {:just2, [], [{:a, [], Elixir}, {:b, [], Elixir}]}  # just2(a, b)
-  # pat = {:just, [], [{{:a, [], Elixir}, {:b, [], Elixir}}]} # just({a, b})
   defmacro pattern(_syn = {:=, _, [lhs, pat]}) do
     {name, meta, args} = lhs
     quote do
@@ -104,8 +259,6 @@ defmodule PatternMetonyms do
   end
 
   # unidirectional
-  # lhs = {:head, [], [{:x, [], Elixir}]}                  # head(x)
-  # pat = [{:|, [], [{:x, [], Elixir}, {:_, [], Elixir}]}] # [x | _]
   defmacro pattern({:<-, _, [lhs, pat]}) do
     {name, meta, args} = lhs
     quote do
@@ -155,11 +308,6 @@ defmodule PatternMetonyms do
   end
 
   # explicit bidirectional / with view
-  # lhs = {:polar, [], [{:r, [], Elixir}, {:a, [], Elixir}]}       # polar(r, a)
-  # fun = {:pointPolar, [], Elixir}                                # pointPolar
-  # pat = {{:r, [], Elixir}, {:a, [], Elixir}}                     # {r, a}
-  # lhs2 = {:polar, [], [{:r, [], Elixir}, {:a, [], Elixir}]}      # polar(r, a)
-  # expr = {:polarPoint, [], [{:r, [], Elixir}, {:a, [], Elixir}]} # polarPoint(r, a)
   defmacro pattern({:when, _, [{:<-, _, [lhs, view = [{:->, _, [[_], pat]}]]}, {:=, _, [lhs2, expr]}]}) do
     {name, meta, args} = lhs
     {^name, _meta2, args2} = lhs2
@@ -222,6 +370,17 @@ defmodule PatternMetonyms do
 
   # view
 
+  @doc """
+  Macro substitute for `case/2` capable of using pattern metonyms
+
+  Custom `case` able to use pattern metonyms defined with this module.
+  Largely unoptimized, try to avoid side effect in your pattern definition as using them multiple time
+  in `view` will repeat them, but might not later on.
+
+  view pattern (`(function -> pattern)`) may be used raw in here.
+
+  Currently does not support either remote calls or anonymous functions.
+  """
   defmacro view(data, do: clauses) when is_list(clauses) do
     [last | rev_clauses] = Enum.reverse(clauses)
 
@@ -249,6 +408,7 @@ defmodule PatternMetonyms do
     #|> case do x -> _ = IO.puts("view:\n#{Macro.to_string(x)}") ; x end
   end
 
+  @doc false
   def view_folder({:->, _, [[[{:->, _, [[{name, meta, nil}], pat]}]], rhs]}, acc, data, _caller_env) do
     call = {name, meta, [data]}
     quote do
@@ -298,6 +458,7 @@ defmodule PatternMetonyms do
 
   # Utils
 
+  @doc false
   def ast_var?({name, meta, con}) when is_atom(name) and is_list(meta) and is_atom(con), do: true
   def ast_var?(_), do: false
 end
